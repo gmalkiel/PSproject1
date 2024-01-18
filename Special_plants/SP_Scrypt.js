5b7f6a0c7ad6bbe98c911ba633347b6d60ace5b0
gmalkiel <galit2944@gmail.com>
2024-01-18 11:08:57 +0200

    1108

--- Special_plants/SP_Scrypt.js
+++ Special_plants/SP_Scrypt.js
@@ -1,33 +1,4 @@
-
-// Toggle grid padding
-function myFunction() {
-    var x = document.getElementById("myGrid");
-    if (x.className === "w3-row") {
-      x.className = "w3-row-padding";
-    } else { 
-      x.className = x.className.replace("w3-row-padding", "w3-row");
-    }
-  }
-  
-  // Open and close sidebar
-  function w3_open() {
-    document.getElementById("mySidebar").style.width = "100%";
-    document.getElementById("mySidebar").style.display = "block";
-  }
-  
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
-
-// Modal Image Gallery
-/*function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-  }*/
-
-  function openLightbox(imageSrc) {
+function openLightbox(imageSrc) {
     document.getElementById('lightbox-image').src = imageSrc;
     document.getElementById('lightbox').style.display = 'flex';
 }
@@ -39,16 +10,3 @@ function closeLightbox() {
 function openExternalLink(url) {
   window.open(url, '_blank');
 }
-
-  
-  /*
-  // Used to toggle the menu on small screens when clicking on the menu button
-  function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-  */
