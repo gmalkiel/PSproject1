1a39b14bcdb8a74753b4538ddb12a95d4ac6e992
gmalkiel <galit2944@gmail.com>
2024-01-18 09:11:10 +0200

    0911

--- DistributionAreasPage.js
+++ DistributionAreasPage.js
@@ -1,5 +1,5 @@
 
-  const scene = new THREE.Scene();
+  /*const scene = new THREE.Scene();
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -10,7 +10,7 @@
   const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
   const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
   const sphere = new THREE.Mesh( geometry, material ); 
-  scene.add( sphere );
+  scene.add( sphere );*/
 
   
   
